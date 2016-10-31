c3cebea3f844e3d7eaefda750fae6e59d3f421a5
Nikolay Stoykov <nikolaisstoykov@gmail.com>
2016-10-31 19:47:54 +0200

    Add initial code

---
+++ index.js
@@ -0,0 +1,100 @@
+'use strict';
+
+var spawn = require('child_process').spawn;
+var readline = require('readline');
+
+var bufferedStdOutput = [];
+var bufferedStdError = [];
+
+var filterOn = '';
+var outputPaused = false;
+
+var params = process.argv.slice(3);
+
+var running = spawn(process.argv[2], params);
+
+
+running.stdout.setEncoding('utf8');
+
+running.stdout.on('data', (data) => {
+    if (outputPaused) {
+        bufferedStdOutput.push(data);
+        return;
+    }
+
+    if (filter(data, filterOn)) {
+        console.log(data);
+    }
+});
+
+running.stderr.on('data', (data) => {
+    if (outputPaused) {
+        bufferedStdError.push(data);
+        return;
+    }
+
+    if (filter(data, filterOn)) {
+        console.log(data);
+    }
+});
+
+running.on('close', (code) => {
+    console.log('[Main] child process exited with code ', code);
+    process.exit(0);
+});
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.on('keypress', function (chunk, key) {
+    if (key.sequence === '\u001b') {
+        if (outputPaused) {
+            // process.stdin.pause();
+            console.log('[Main] Output resumed');
+            processBufferedOutput();
+            process.exit(0);
+        } else {
+            process.stdin.resume();
+            console.log('[Main] Output paused');
+        }
+        outputPaused = !outputPaused;
+    }
+});
+
+var input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+input.on('line', (line) => {
+    if (outputPaused) {
+        filterOn = line;
+        console.log('[Main] Output will be filtered on => ', filterOn);
+    }
+});
+
+function processBufferedOutput() {
+    var output = bufferedStdOutput.shift();
+    while(output) {
+        if (filter(output, filterOn)) {
+            console.log(output);
+        }
+        output = bufferedStdOutput.shift();
+    }
+
+    output = bufferedStdError.shift()
+    while(output && filter(output, filterOn)) {
+        if (filter(output, filterOn)) {
+            console.err(output);
+        }
+        output = bufferedStdError.shift();
+    }
+}
+
+function filter(message, filterText) {
+    if (!message) {
+        return false;
+    }
+    if (message.indexOf(filterText) >= 0) {
+        return true;
+    }
+    return false;
+}
